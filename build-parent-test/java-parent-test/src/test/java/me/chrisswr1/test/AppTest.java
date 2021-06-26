a741a7fc349090aa46aa85326d0c1cef35b6a25a
ChrissW-R1 <contact@ChrissW-R1.me>
2021-06-26 22:51:10 +0200

    added suite to test build-parents

---
+++ build-parent-test/java-parent-test/src/test/java/me/chrisswr1/test/AppTest.java
@@ -0,0 +1,17 @@
+package me.chrisswr1.test;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+/**
+ * unit test for main class
+ */
+public class AppTest {
+	/**
+	 * basic test, which is always true
+	 */
+	@Test
+	public void shouldAnswerWithTrue() {
+		Assertions.assertTrue(true);
+	}
+}
