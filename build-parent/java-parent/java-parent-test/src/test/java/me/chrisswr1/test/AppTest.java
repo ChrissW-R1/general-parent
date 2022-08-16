96eafc08679845600f42ed5ba3497361326d2dc8
ChrissW-R1 <contact@ChrissW-R1.me>
2022-08-16 10:38:13 +0200

    implemented unit tests in java-parent-test module

--- build-parent/java-parent/java-parent-test/src/test/java/me/chrisswr1/test/AppTest.java
+++ build-parent/java-parent/java-parent-test/src/test/java/me/chrisswr1/test/AppTest.java
@@ -5,13 +5,17 @@ import org.junit.jupiter.api.Test;
 
 /**
  * unit test for main class
+ *
+ * @since 1.0.0
  */
 public class AppTest {
 	/**
 	 * basic test, which is always true
+	 *
+	 * @since 1.0.0
 	 */
 	@Test
-	public void shouldAnswerWithTrue() {
-		Assertions.assertTrue(true);
+	public void testHelloWorld() {
+		Assertions.assertEquals("Hello World!", App.helloWorld());
 	}
 }
