cae0037a4e020fa7cb83ceb185acc9913e24dc65
ChrissW-R1 <contact@ChrissW-R1.me>
2024-02-13 23:40:42 +0100

    fixed path to generated webpack configuration

--- build-parent/js-parent/src/main/node/Gruntfile.js
+++ build-parent/js-parent/src/main/node/Gruntfile.js
@@ -1,61 +1,64 @@
-const webpackConfig = require(
-	"${project.module-root.relativedir}/webpack.config.js"
-);
+const webpackConfig = require("./webpack.config.js");
 
 module.exports = function(grunt) {
 	// Project configuration
-	grunt.initConfig({
-						 pkg:     grunt.file.readJSON("package.json"),
-						 eslint:  {
-							 options: {
-								 overrideConfigFile: ".eslintrc.json"
-							 },
-							 target:  [
-								 "${project.build.sourceDirectory}/**/*.ts"
-							 ]
-						 },
-						 ts:      {
-							 options: {
-								 rootDir: "${project.build.sourceDirectory}"
-							 },
-							 main:    {
-								 tsconfig: true,
-								 src:      "${project.build.sourceDirectory}/**/*.ts",
-								 dest:     "${project.build.directory}/generated-sources/ts-compiled"
-							 }
-						 },
-						 uglify:  {
-							 options: {
-								 sourceMap: true
-							 },
-							 main:    {
-								 files: [
-									 {
-										 expand: true,
-										 cwd:    "${project.build.directory}/generated-sources/ts-compiled",
-										 src:    "**/*.js",
-										 dest:   "${project.build.directory}/generated-sources/uglified",
-										 ext:    ".min.js"
-									 }
-								 ]
-							 }
-						 },
-						 webpack: {
-							 myConfig: webpackConfig
-						 }
-					 });
+	grunt.initConfig(
+		{
+			pkg:     grunt.file.readJSON("package.json"),
+			eslint:  {
+				options: {
+					overrideConfigFile: ".eslintrc.json"
+				},
+				target:  [
+					"${project.build.sourceDirectory}/**/*.ts"
+				]
+			},
+			ts:      {
+				options: {
+					rootDir: "${project.build.sourceDirectory}"
+				},
+				main:    {
+					tsconfig: true,
+					src:      "${project.build.sourceDirectory}/**/*.ts",
+					dest:     "${project.build.directory}/generated-sources/ts-compiled"
+				}
+			},
+			uglify:  {
+				options: {
+					sourceMap: true
+				},
+				main:    {
+					files: [
+						{
+							expand: true,
+							cwd:    "${project.build.directory}/generated-sources/ts-compiled",
+							src:    "**/*.js",
+							dest:   "${project.build.directory}/generated-sources/uglified",
+							ext:    ".min.js"
+						}
+					]
+				}
+			},
+			webpack: {
+				myConfig: webpackConfig
+			}
+		}
+	);
 
 	// Load plugins
 	grunt.loadNpmTasks("grunt-eslint");
 	grunt.loadNpmTasks("grunt-ts");
 	grunt.loadNpmTasks("grunt-contrib-uglify");
-	grunt.loadNpmTasks('grunt-webpack');
+	grunt.loadNpmTasks("grunt-webpack");
 
 	// Default task(s)
-	grunt.registerTask("default", [
-		"eslint",
-		"ts",
-		"uglify",
-		"webpack"
-	]);
+	grunt.registerTask(
+		"default",
+		[
+			"eslint",
+			"ts",
+			"uglify",
+			"webpack"
+		]
+	);
 };
