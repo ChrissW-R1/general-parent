2b74dcf65a6eacecd77b6b963f4d566512b4dab0
ChrissW-R1 <contact@ChrissW-R1.me>
2024-02-14 03:24:27 +0100

    generate paths for webpack process

--- build-parent/js-parent/src/main/node/webpack.config.js
+++ build-parent/js-parent/src/main/node/webpack.config.js
@@ -1,10 +1,10 @@
-const path = require('path');
+const path = require("path");
 
 module.exports = {
-	mode:   'production',
-	entry:  `${project.build.directory}/generated-sources/uglified/app.min.js`,
+	mode:   "production",
+	entry:  "${project.build.directory}/generated-sources/uglified/app.min.js",
 	output: {
-		path:     path.resolve(__dirname, 'target/dist'),
-		filename: 'app.js'
+		path:     path.resolve(__dirname, "${project.build.directory}/dist/js"),
+		filename: "app.js"
 	}
 };
