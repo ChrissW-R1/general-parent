602fac906a9f1fbc435530466b498a990f4e8e85
ChrissW-R1 <contact@ChrissW-R1.me>
2025-07-31 04:32:08 +0200

    added log statements to Java source code

--- build-parent/java-parent/webapp-parent/webapp-parent-test/src/main/java/me/chrisswr1/test/webapp/BaseServlet.java
+++ build-parent/java-parent/webapp-parent/webapp-parent-test/src/main/java/me/chrisswr1/test/webapp/BaseServlet.java
@@ -3,17 +3,20 @@ package me.chrisswr1.test.webapp;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import lombok.extern.slf4j.Slf4j;
+import org.jetbrains.annotations.Nullable;
 
 import java.io.IOException;
 
 /**
  * {@link HttpServlet}, which merges the request methods GET and POST
  *
- * @version 1.4.0
+ * @version 2.5.0
  * @since 1.0.0
  */
+@Slf4j
 public abstract class BaseServlet
-	extends HttpServlet {
+extends HttpServlet {
 	/**
 	 * Handles a request. Independent to the method (GET or POST).
 	 *
@@ -21,31 +24,38 @@ public abstract class BaseServlet
 	 * @param response the {@link HttpServletResponse},
 	 *                 which will be sent to the client
 	 * @throws IOException if any in- or output {@link Exception} occurred
+	 *
 	 * @see BaseServlet#doGet(HttpServletRequest, HttpServletResponse)
 	 * @see BaseServlet#doPost(HttpServletRequest, HttpServletResponse)
+	 *
 	 * @since 1.0.0
 	 */
 	protected abstract void doRequest(
-		HttpServletRequest request,
-		HttpServletResponse response
-	)
-	throws IOException;
+		final @Nullable HttpServletRequest request,
+		final @Nullable HttpServletResponse response
+	) throws IOException;
 
 	@Override
 	protected void doGet(
-		HttpServletRequest request,
-		HttpServletResponse response
-	)
-	throws IOException {
+		final @Nullable HttpServletRequest request,
+		final @Nullable HttpServletResponse response
+	) throws IOException {
+		BaseServlet.log.trace(
+			"Handle GET request: {}",
+			request.getRequestURI()
+		);
 		this.doRequest(request, response);
 	}
 
 	@Override
 	protected void doPost(
-		HttpServletRequest request,
-		HttpServletResponse response
-	)
-	throws IOException {
+		final @Nullable HttpServletRequest request,
+		final @Nullable HttpServletResponse response
+	) throws IOException {
+		BaseServlet.log.trace(
+			"Handle POST request: {}",
+			request.getRequestURI()
+		);
 		this.doRequest(request, response);
 	}
 }
--- build-parent/java-parent/webapp-parent/webapp-parent-test/src/main/java/me/chrisswr1/test/webapp/MainServlet.java
+++ build-parent/java-parent/webapp-parent/webapp-parent-test/src/main/java/me/chrisswr1/test/webapp/MainServlet.java
@@ -5,7 +5,9 @@ import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.Getter;
+import lombok.extern.slf4j.Slf4j;
 import me.chrisswr1.test.App;
+import org.jetbrains.annotations.Nullable;
 
 import java.io.IOException;
 
@@ -13,14 +15,13 @@ import java.io.IOException;
  * {@link Servlet}, which handles all {@link HttpServletRequest}s
  * on the root path
  *
- * @version 1.4.0
+ * @version 2.5.0
  * @since 1.0.0
  */
+@Slf4j
 @WebServlet(
 	description = "handles the HTTP request",
-	urlPatterns = {
-		"/*"
-	}
+	urlPatterns = {"/*"}
 )
 public class MainServlet
 extends BaseServlet {
@@ -34,10 +35,10 @@ extends BaseServlet {
 
 	@Override
 	protected void doRequest(
-		HttpServletRequest request,
-		HttpServletResponse response
-	)
-	throws IOException {
+		final @Nullable HttpServletRequest request,
+		final @Nullable HttpServletResponse response
+	) throws IOException {
+		MainServlet.log.trace("Handling request: {}", request.getRequestURI());
 		response.getWriter().println(App.helloWorld());
 	}
 }
