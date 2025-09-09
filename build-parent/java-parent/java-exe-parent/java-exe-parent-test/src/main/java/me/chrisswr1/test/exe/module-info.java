dc6e527b42395278a7cb4c349494721b414c8dc4
ChrissW-R1 <contact@ChrissW-R1.me>
2025-09-09 14:28:13 +0200

    adds some necessary modules to module information

--- build-parent/java-parent/java-exe-parent/java-exe-parent-test/src/main/java/me/chrisswr1/test/exe/module-info.java
+++ build-parent/java-parent/java-exe-parent/java-exe-parent-test/src/main/java/me/chrisswr1/test/exe/module-info.java
@@ -1,2 +1,3 @@
 module me.chrisswr1.test.exe {
+	requires me.chrisswr1.test;
 }
--- build-parent/java-parent/java-parent-test/src/main/java/me/chrisswr1/test/module-info.java
+++ build-parent/java-parent/java-parent-test/src/main/java/me/chrisswr1/test/module-info.java
@@ -1,5 +1,8 @@
 module me.chrisswr1.test {
-	requires org.jetbrains.annotations;
-	requires lombok;
+	requires static org.jetbrains.annotations;
+	requires static lombok;
+
 	requires org.slf4j;
+
+	exports me.chrisswr1.test;
 }
